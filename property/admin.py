5ecdb5a6f04ab1537ac115ec9dd9ad849951ffe8
Андрей Кустов <andrey_kustov@MacBook-Pro-Andrej.local>
2022-10-20 12:32:02 +0300

    add owners to flat in admin

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,16 @@ from django.contrib import admin
 from .models import Flat, Complain, Owner
 
 
+@admin.register(Owner)
+class OwnerAdmin(admin.ModelAdmin):
+    raw_id_fields = ('flats',)
+
+
+class OwnerInline(admin.StackedInline):
+    model = Owner.flats.through
+    raw_id_fields = ('owner',)
+
+
 @admin.register(Flat)
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
@@ -10,13 +20,10 @@ class FlatAdmin(admin.ModelAdmin):
     list_display = ('address', 'new_building', 'construction_year')
     list_editable = ('new_building',)
     list_filter = ('new_building',)
+    inlines = (OwnerInline,)
     raw_id_fields = ('liked_by',)
+
 
 @admin.register(Complain)
 class ComplainAdmin(admin.ModelAdmin):
     raw_id_fields = ('flat',)
-
-
-@admin.register(Owner)
-class OwnerAdmin(admin.ModelAdmin):
-    raw_id_fields = ('flats',)
